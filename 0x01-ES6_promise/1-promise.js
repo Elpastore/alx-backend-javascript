bf81f77cb22c8916ec797dbf535bae9aaf3d206b
elpastore <abdoulayesadio1997@gmail.com>
2024-04-24 21:53:31 +0000

    task1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const data = {
+        status: '200',
+        body: 'Sucess',
+      };
+      resolve(data);
+    } else {
+      const error = new Error('The fake API is not working currently');
+      reject(error);
+    }
+  });
+}
