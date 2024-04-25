5b040fd5dc88820144d0a5c75a35165077392c02
elpastore <abdoulayesadio1997@gmail.com>
2024-04-25 21:22:21 +0000

    task2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('lenght must be a number');
+    }
+    this._length = value;
+  }
+
+  get student() {
+    return this._student;
+  }
+
+  set student(value) {
+    if (!Array.isArray(value)) {
+      throw new TypeError('student must be an array');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('item in the array  must be a string');
+    }
+    this._student = value;
+  }
+}
