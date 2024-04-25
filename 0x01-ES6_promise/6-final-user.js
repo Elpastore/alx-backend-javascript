87e8594b7bf88c701623c0673efc29b029b8e6a0
elpastore <abdoulayesadio1997@gmail.com>
2024-04-25 00:08:21 +0000

    task6_v3

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([userPromise, photoPromise])
     .then((results) => results.map((result) => {
       if (result.status === 'fulfilled') {
-        return { status: 'fulfilled', value: String(result.value) };
+        return { status: 'fulfilled', value: result.value };
       }
       return { status: 'rejected', reason: String(result.reason) };
     }));
