1f6ebeb78b44ff590475d4f1ec068a4a1e0a261e
elpastore <abdoulayesadio1997@gmail.com>
2024-04-29 23:02:30 +0000

    task0_v1

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -7,7 +7,7 @@ export default function getListStudents() {
   }
   const student1 = new Student(1, 'Guillaume', 'San Francisco');
   const student2 = new Student(2, 'James', 'Columbia');
-  const student3 = new Student(3, 'Serena', 'San Francisco');
+  const student3 = new Student(5, 'Serena', 'San Francisco');
   listStudents.push(student1, student2, student3);
   return listStudents;
 }
