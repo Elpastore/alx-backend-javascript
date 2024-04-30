bec552fdb194c54f93cbea95d2f3368a019514eb
elpastore <abdoulayesadio1997@gmail.com>
2024-04-30 21:21:54 +0000

    task8_v4

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@ export default function cleanSet(set, startString) {
   }
   const concate = [];
   for (const value of set) {
-    if (typeof value === 'string'&& value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       const restChar = value.slice(startString.length);
       concate.push(restChar);
     }
