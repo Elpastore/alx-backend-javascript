44c13a698a4f6f1606d44ca06ea59e869cb1f523
elpastore <abdoulayesadio1997@gmail.com>
2024-06-18 16:41:43 +0000

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,13 @@
-const readLine = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const yourInput = process.stdin.read();
+
+  if (yourInput) {
+    process.stdout.write(`Your name is: ${yourInput}`);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (input) => {
-  console.log(`Your name is ${input}`);
-  rl.close();
-});
-
-rl.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
