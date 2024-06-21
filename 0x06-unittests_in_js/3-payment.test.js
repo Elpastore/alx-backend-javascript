17bac7914c507febdba2b67a639b24ae75d2961a
elpastore <abdoulayesadio1997@gmail.com>
2024-06-21 14:59:27 +0000

    task4

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,15 @@
-const sinon = require('sinon');
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
-const expect = require('chai');
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
+const assert = require("assert");
 
-describe('sendPaymentRequestToApi', function () {
-  it('should call Utils.calculateNumber with SUM, 100, 20', function () {
-    const spy = sinon.spy(Utils, 'calculateNumber');
+describe("sendPaymentRequestToApi", function() {
+    it("check that Utils.calculateNumber was called once", function() {
+	const spy = sinon.spy(Utils, "calculateNumber");
 
-    sendPaymentRequestToApi(100, 20);
+	sendPaymentRequestToApi(50, 24.52);
 
-    expect(spy.calledOnce).to.be.true;
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
-    spy.restore();
-  });
+	assert(spy.calledOnce);
+	spy.restore();
+    });
 });
