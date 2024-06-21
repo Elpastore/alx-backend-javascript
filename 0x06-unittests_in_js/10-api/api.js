30d23e1ffa596d826f6979d25b94f2f442c376da
elpastore <abdoulayesadio1997@gmail.com>
2024-06-21 20:55:24 +0000

    tassk10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,34 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 7865;
+
+
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
   response.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id([0-9]+)', (request, response) => {
-    response.send(`Payment methods for cart ${request.params.id}`);
+app.get('/cart/:id(\\d+)', (request, response) => {
+  response.send(`Payment methods for cart ${request.params.id}`);
 });
 
 app.get('/available_payments', (request, response) => {
-    response.set("Content-Type", "application/json");
-    const payMethods = {
-	payment_methods: {
-          credit_cards: true,
-          paypal: false
-	}
+  const payMethods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
     }
-    response.send(payMethods);
+  };
+  response.json(payMethods);
 });
 
 app.post('/login', (req, res) => {
-    const { userName } = req.body;
-    res.send(`Welcome ${userName}`);
-  });
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
+});
 
 app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
+  console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
