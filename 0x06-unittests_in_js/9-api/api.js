79418170fa2db6c9032a67959fdf598a77238fc3
elpastore <abdoulayesadio1997@gmail.com>
2024-06-21 20:30:36 +0000

    task9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -7,13 +7,12 @@ app.get('/', (request, response) => {
   response.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (request, response) => {
-  const id = request.params.id;
-  response.send(`Payment methods for cart :${id}`);
+app.get('/cart/:id([0-9]+)', (request, response) => {
+    response.send(`Payment methods for cart ${request.params.id}`);
 });
 
 app.listen(port, () => {
-  console.log("API available on localhost port 7865");
+    console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
