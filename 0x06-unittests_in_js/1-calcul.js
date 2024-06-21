3590480a69900e930348720080e65349db21b043
elpastore <abdoulayesadio1997@gmail.com>
2024-06-21 00:03:54 +0000

    task1

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,18 +1,18 @@
-function calculateNumber(type, a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
-    if (type === "SUM") {
-      return roundedA + roundedB;
+function calculateNumber (type, a, b) {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+  if (type === 'SUM') {
+    return roundedA + roundedB;
+  }
+  if (type === 'SUBSTRACT') {
+    return roundedA - roundedB;
+  }
+  if (type === 'DIVIDE') {
+    if (b === 0) {
+      return 'Error';
     }
-    if (type === "SUBSTRACT") {
-      return roundedA - roundedB;
-    }
-    if (type === "DIVIDE") {
-      if (b === 0) {
-        return "Error";
-      }
-      return roundedA / roundedB;
-    }
+    return roundedA / roundedB;
+  }
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,27 +1,26 @@
 const calculateNumber = require('./1-calcul');
 const assert = require('assert');
 
-
-describe("CalculateNumber test", function() {
-  describe("Add test case", function() {
-    it('test if type is SUM', function() {
-        assert.strictEqual(calculateNumber("SUM", 1.2, 4), 5);
-      });
+describe('CalculateNumber test', function () {
+  describe('Add test case', function () {
+    it('test if type is SUM', function () {
+      assert.strictEqual(calculateNumber('SUM', 1.2, 4), 5);
+    });
   });
 
-  describe("SuBSTRACT test case", function() {
-    it('test if type is SUBSTRACT', function() {
-        assert.strictEqual(calculateNumber("SUBSTRACT", 1.2, -4), 5);
-      });
+  describe('SuBSTRACT test case', function () {
+    it('test if type is SUBSTRACT', function () {
+      assert.strictEqual(calculateNumber('SUBSTRACT', 1.2, -4), 5);
+    });
   });
 
-  describe("DIVIDE test case", function() {
-    it('test if type is DIVIDE', function() {
-        assert.strictEqual(calculateNumber("DIVIDE", 10, 4.7), 2);
-      });
+  describe('DIVIDE test case', function () {
+    it('test if type is DIVIDE', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 10, 4.7), 2);
+    });
 
-      it('DIVIDE test case', function() {
-        assert.strictEqual(calculateNumber("DIVIDE", 10, 0), "Error");
-      });
+    it('DIVIDE test case', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 10, 0), 'Error');
+    });
   });
 });
