0c77709ec1681fd8c556ccac890d554c31c4fe6a
elpastore <abdoulayesadio1997@gmail.com>
2024-06-21 13:13:34 +0000

    task0_redo

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,31 +1,17 @@
 const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
-describe("Test case", function() {
-  it("test if 1.2 and 5 is equal to 5", function() {
-    assert.strictEqual(calculateNumber(1.2, 4), 5);
+describe('Test cases', function() {
+  it('test 1', function() {
+    assert(calculateNumber(1, 3), 4);
   });
-  it("test if 1.8 and 4.6 is equal to 6", function() {
-    assert.strictEqual(calculateNumber(1.8, 4.6), 7);
+  it('test 2', function() {
+    assert(calculateNumber(1, 3.7), 5);
   });
-
-  it("test if 2 and 3 is equal to 5", function() {
-    assert.strictEqual(calculateNumber(2, 3), 5);
+  it('test 3', function() {
+    assert(calculateNumber(1.2, 3.7), 5);
   });
-
-  it("test if 0 and 0 is equal to 0", function() {
-    assert.strictEqual(calculateNumber(0, 0), 0);
-  });
-
-  it("test if negatif number works", function() {
-    assert.strictEqual(calculateNumber(-2, -7.8), -10);
-  });
-
-  it("test if negatif and positif number wors", function() {
-    assert.strictEqual(calculateNumber(2.8, -8), -5);
-  });
-
-  it("test if negatif and positif number wors", function() {
-    assert.strictEqual(calculateNumber(8, -2.8), 5);
+  it('test 4', function() {
+    assert(calculateNumber(1.5, 3.7), 6);
   });
 })
